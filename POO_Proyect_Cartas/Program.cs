ae2a1da29e25b1f22d93141ed7e5c7a6764fc037
Diegoyhulk <diegoyhulk@icloud.com>
2026-03-22 11:33:04 +0100

    Initial commit

---
+++ POO_Proyect_Cartas/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace POO_Proyect_Cartas;
+
+class Program
+{
+    private Partida partida;
+    private static bool win = false;
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+
+            if (win){break;}
+        }
+    }
+}
