8437f0217a8a4089e55f22888111bb039daaa8da
Diegoyhulk <diegoyhulk@icloud.com>
2026-03-29 20:26:27 +0200

    a

--- POO_Proyect_Cartas/obj/Debug/net8.0/POO_Proyect_Cartas.AssemblyInfo.cs
+++ POO_Proyect_Cartas/obj/Debug/net8.0/POO_Proyect_Cartas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO_Proyect_Cartas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f550993c995f8fdbc95fb75e1c30aed6fc23644f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b3384e81794ac7888b4dfcbcff0ceca90d693e")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO_Proyect_Cartas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO_Proyect_Cartas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
